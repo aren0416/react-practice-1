45e95e06df07ff8fcbc335e0ecc60aa0538d1682
aren0416 <aren0416@gmail.com>
2022-04-05 19:45:03 +0900

    route 경로 수정

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { Home } from "./components/home/Home";
 import { PageNotFound } from "./components/PageNotFound";
 import { Search } from "./components/search/Search";
 import { GlobalStyles } from "./components/styles/GlobalStyled";
+import { routes } from "./routes";
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail" element={<Detail />}></Route>
-        <Route path="/search" element={<Search />}></Route>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.detail} element={<Detail />}></Route>
+        <Route path={routes.search} element={<Search />}></Route>
         <Route path="/*" element={<PageNotFound />}></Route>
       </Routes>
     </Router>
